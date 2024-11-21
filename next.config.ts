74240507ebf0f41a54c75adf7d96a89db6d79de2
Aaron Ogbemi <aawonny@gmail.com>
2024-11-21 08:36:49 +0100

    Deployment support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: isProd ? 'https://lexykapp.com/' : '',
+  basePath: isProd ? '' : '',
+  output: 'export',
 };
 
 export default nextConfig;
