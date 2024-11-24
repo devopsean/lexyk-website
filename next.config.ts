1b38caa0aa372aab707966892d974faf50cec0a2
Aaron Ogbemi <aawonny@gmail.com>
2024-11-24 12:48:52 +0100

    Removed gh-pages changes

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   /* config options here */
-  assetPrefix: isProd ? 'https://lexykapp.com/' : '',
-  basePath: isProd ? '' : '',
-  output: 'export',
-  images: {
-    unoptimized: true, // Disable Image Optimization API
-  },
 };
 
 export default nextConfig;
