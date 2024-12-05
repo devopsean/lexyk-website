36072d6a44bbb2f176269bdecefa6fd86b90e195
Aaron Ogbemi <aawonny@gmail.com>
2024-12-05 01:18:33 +0100

    Font size edit

--- src/app/sections/getstarted.tsx
+++ src/app/sections/getstarted.tsx
@@ -18,7 +18,7 @@ function Getstarted() {
           <h2 className="text-2xl lg:text-4xl xl:text-6xl 2xl:text-7xl over-2000:text-[3.5vw] lg:mx-4 xl:mx-6 lg:my-5 xl:my-7 md:text-5xl font-bold mb-6 flex items-center justify-center gap-4">
             {STRINGS.section4}
           </h2>
-          <p className="text-white text-lg lg:text-2xl xl:text-3xl 2xl:text-4xl over-2000:text-[1.25vw]/[2vw] max-w-2xl over-2000:max-w-[40vw] mx-auto mb-8">
+          <p className="text-white text-2xl over-2000:text-[1.25vw]/[2vw] max-w-2xl over-2000:max-w-[40vw] mx-auto font-medium mx-auto mb-8">
             {STRINGS.sectionabt4}
           </p>
           <button className="bg-white text-black rounded-xl over-2000:rounded-[0.5vw] hover:bg-white/90 px-8 py-4 over-2000:py-[1vw] over-2000:px-[1.25vw] over-2000:text-[1.25vw]">
