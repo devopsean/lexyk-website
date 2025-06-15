34ceecc74d551301d7e0d2d315bf98dfb2d0d10d
devopsean <seanonuoha@gmail.com>
2025-06-14 22:08:50 -0400

    fixed errors

--- src/app/components/googleplaystore.tsx
+++ src/app/components/googleplaystore.tsx
@@ -1,13 +1,20 @@
-import React from "react";
-import { PlayStore } from "react-app-store-badges";
+import MobileStoreButton from 'react-mobile-store-button';
+import React from 'react';
 
-export default function GooglePlayButton() {
-  return (
-    <PlayStore
-      link="https://play.google.com/store/apps/details?id=com.lexyk.lexyk"
-      width={200}           // scale as you like
-      className="cursor-pointer"
-    />
-  );
+////
+
+export default class MyComponent extends React.Component {
+	render() {
+		const iOSUrl = 'https://itunes.apple.com/us/app/all-of-the-lights/id959389722?mt=8';
+		return (
+			<div>
+				<MobileStoreButton
+				  store="ios"
+				  url={iOSUrl}
+				  linkProps={{ title: 'iOS Store Button' }}
+				/>
+			</div>
+		);
+	}
 }
 
