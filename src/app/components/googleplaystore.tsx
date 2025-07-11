d731f605e13d8be0dfc7deee74db97e0086c95a3
devopsean <seanonuoha@gmail.com>
2025-07-11 10:54:00 -0400

    updated playstoer lin

--- src/app/components/googleplaystore.tsx
+++ src/app/components/googleplaystore.tsx
@@ -3,9 +3,9 @@ import React from "react";
 function Googleplaystore() {
   return (
     <a
-      href="https://play.google.com/store/apps/details?id=YOUR_APP_PACKAGE_NAME" // <--- IMPORTANT: Replace YOUR_APP_PACKAGE_NAME
-      target="_blank" // Opens the link in a new tab
-      rel="noopener noreferrer" // Recommended for security when using target="_blank"
+      href="https://play.google.com/store/apps/details?id=com.lexyk.lexyk" 
+      target="_blank" 
+      rel="noopener noreferrer"
       className="flex w-48 h-14 md:min-w-4 lg:w-64 lg:h-16 bg-black text-white rounded-xl over-2000:rounded-[0.8vw] items-center justify-center border border-white over-2000:min-w-[15vw] over-2000:h-auto over-2000:pb-3 over-2000:pt-2"
     >
       <div className="mr-3">
